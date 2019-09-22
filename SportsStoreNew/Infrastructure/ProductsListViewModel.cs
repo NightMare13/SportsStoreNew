5a8954515eb160280f052651a228df2e2de146ce
Павел Дмитриев <PavelDmitriev98@yandex.ru>
2019-09-22 21:21:55 +0300

    8 chapter

---
+++ SportsStoreNew/Infrastructure/ProductsListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using SportsStoreNew.Models;
+using SportsStoreNew.Models.ViewModels;
+
+namespace SportsStoreNew.Infrastructure
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
--- SportsStoreNew/Startup.cs
+++ SportsStoreNew/Startup.cs
@@ -5,16 +5,28 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SportsStoreNew.Models;
 
 namespace SportsStoreNew
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddTransient<IProductRepository, FakeProductRepository>();
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration["Data:SportStoreProducts:ConnectionString"]));
+            services.AddTransient<IProductRepository, EFProductRepository>();
             services.AddMvc();
         }
 
@@ -28,7 +40,18 @@ namespace SportsStoreNew
 
             app.UseStatusCodePages();
             app.UseStaticFiles();
-            app.UseMvc(routes => { });
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "pagination",
+                    template: "Products/Page{productPage}",
+                    defaults: new {Controller = "Product", action = "List"});
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Product}/{action=List}/{id?}");
+            });
+
+            SeedData.EnsurePopulated(app);
         }
     }
 }
